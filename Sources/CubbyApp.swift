87ec1ff2592339a0fd34f8ab22d5c792bdb2e79d
SalzDevs <a49000@alunos.isel.pt>
2026-09-17 16:29:46 +0100

    Green button: native zoom instead of fullscreen

--- Sources/CubbyApp.swift
+++ Sources/CubbyApp.swift
@@ -7,10 +7,24 @@ struct CubbyApp: App {
             ContentView()
                 .frame(minWidth: 320, minHeight: 420)
                 .background(Color.black)
+                .background(WindowAccessor())
         }
         .windowStyle(.hiddenTitleBar)
         .windowResizability(.contentMinSize)
     }
+}
+
+// Makes green traffic-light button zoom (expand arrows) instead of fullscreen
+struct WindowAccessor: NSViewRepresentable {
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        DispatchQueue.main.async {
+            view.window?.collectionBehavior = [.fullScreenAuxiliary]
+        }
+        return view
+    }
+
+    func updateNSView(_ nsView: NSView, context: Context) {}
 }
 
 struct ContentView: View {
